47839650961cd8de1fabd8baec7a2f8e4a49e508
Matt Goo <mgoo@transcarent.ai>
2021-06-16 10:42:44 -0700

    init commit

---
+++ pages/_app.tsx
@@ -0,0 +1,161 @@
+import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const brand = {
+  50: '#F4F4F7',
+  100: '#eeecff',
+  500: '#615DB7',
+  900: '#363286',
+}
+const brandSecondary = {
+  50: '#fffadb',
+  100: '#fef1ae',
+  200: '#fce880',
+  300: '#fbdf4f',
+  400: '#F9D412',
+}
+
+const logo = {
+  primaryColorLight: 'white',
+  secondaryColorLight: '#17C7A4',
+  primaryColorDark: '#363286',
+  secondaryColorDark: '#2EA38A',
+}
+
+const fontSizes = {
+  heroMobile: '48px',
+  heroDesktop: '60px',
+  headline1Mobile: '32px',
+  headline1Desktop: '56px',
+  headline2Mobile: '24px',
+  headline2Desktop: '40px',
+  headline3Mobile: '20px',
+  headline3Desktop: '32px',
+  headline4Mobile: '20px',
+  headline4Desktop: '24px',
+  subtitle: '12px',
+  body: '16px',
+  bodyLarge: '18px',
+  bodyLargeSemibold: '18px',
+  bodySemibold: '16px',
+  bodySmall: '14px',
+  bodySmallSemibold: '14px',
+  caption: '10px',
+  captionSemibold: '10px',
+}
+
+const theme = extendTheme({
+  colors: {
+    brand,
+    brandSecondary,
+    logo,
+  },
+  textStyles: {
+    hero: {
+      fontFamily: 'heading',
+      fontSize: [fontSizes.heroMobile, null, fontSizes.heroDesktop],
+      lineHeight: ['64px', null, '80px'],
+      fontWeight: 'semibold',
+    },
+    headline1: {
+      fontFamily: 'heading',
+      fontSize: [fontSizes.headline1Mobile, null, fontSizes.headline1Desktop],
+      lineHeight: ['40px', null, '64px'],
+      fontWeight: 'semibold',
+    },
+    headline2: {
+      fontFamily: 'heading',
+      fontSize: [fontSizes.headline2Mobile, null, fontSizes.headline2Desktop],
+      lineHeight: ['32px', null, '48px'],
+      fontWeight: 'semibold',
+    },
+    headline3: {
+      fontFamily: 'heading',
+      fontSize: [fontSizes.headline3Mobile, null, fontSizes.headline3Desktop],
+      lineHeight: ['24px', null, '36px'],
+      fontWeight: 'semibold',
+    },
+    headline4: {
+      fontFamily: 'body',
+      fontSize: [fontSizes.headline4Mobile, null, fontSizes.headline4Desktop],
+      lineHeight: ['24px', null, '32px'],
+      fontWeight: 'bold',
+    },
+    subtitle: {
+      fontFamily: 'body',
+      fontSize: [fontSizes.subtitle],
+      lineHeight: '16px',
+      letterSpacing: '0.3px',
+      textTransform: 'uppercase',
+    },
+    body: {
+      fontFamily: 'body',
+      fontSize: [fontSizes.body],
+      lineHeight: '24px',
+      fontWeight: 'normal',
+    },
+    bodyLarge: {
+      fontFamily: 'body',
+      fontSize: [fontSizes.bodyLarge],
+      lineHeight: '24px',
+      fontWeight: 'normal',
+    },
+    bodyLargeSemibold: {
+      fontFamily: 'body',
+      fontSize: [fontSizes.bodyLargeSemibold],
+      lineHeight: '24px',
+      fontWeight: 'bold',
+    },
+    bodySemibold: {
+      fontFamily: 'body',
+      fontSize: [fontSizes.bodySemibold],
+      lineHeight: '24px',
+      fontWeight: 'bold',
+    },
+    bodySmall: {
+      fontFamily: 'body',
+      fontSize: [fontSizes.bodySmall],
+      lineHeight: '22px',
+      fontWeight: 'normal',
+    },
+    bodySmallSemibold: {
+      fontFamily: 'body',
+      fontSize: [fontSizes.bodySmallSemibold],
+      lineHeight: '22px',
+      fontWeight: 'bold',
+    },
+    caption: {
+      fontFamily: 'body',
+      fontSize: [fontSizes.caption],
+      lineHeight: '16px',
+      fontWeight: 'normal',
+    },
+    captionSemibold: {
+      fontFamily: 'body',
+      fontSize: [fontSizes.captionSemibold],
+      lineHeight: '16px',
+      fontWeight: 'bold',
+    },
+  },
+  fontWeights: {
+    light: 200,
+    medium: 300,
+    normal: 400,
+    semibold: 500,
+    bold: 600,
+    extrabold: 700,
+  },
+  fontSizes,
+})
+
+console.log(theme)
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
+
+export default MyApp
---
+++ pages/index.tsx
@@ -0,0 +1,116 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import {
+  Flex,
+  chakra,
+  Box,
+  Text,
+  Divider,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+} from '@chakra-ui/react'
+
+const ChakraImage = chakra(Image, {
+  shouldForwardProp: (prop) => {
+    return ['src', 'width', 'height'].includes(prop)
+  },
+  baseStyle: {
+    borderRadius: 'md',
+  },
+})
+
+export default function Home() {
+  return (
+    <Box>
+      <Head>
+        <title>Adios Ryan</title>
+        <meta name="description" content="Gracias and Adios" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Flex bgColor="brand.500" p={2} align="center">
+        <Flex maxW={8} align="center">
+          <ChakraImage
+            src="https://storage.googleapis.com/tc-assets/tc-app-icon.svg"
+            layout="responsive"
+            width="596px"
+            height="596px"
+          />
+        </Flex>
+      </Flex>
+
+      <Box maxW="container.md" p={[4, null, 8]} m="auto">
+        <Box m="auto">
+          <Image width={'350px'} height={'350px'} src="/profile.jpg" />
+          <Flex align="flex-end">
+            <Text textStyle="headline2">Ryan Irilli ,</Text>
+            <Text textStyle="headline3" fontWeight="normal" color="gray.500">
+              21
+            </Text>
+          </Flex>
+        </Box>
+        <Divider my={8} />
+        <Box>
+          <Text color="gray.700">
+            {
+              'Fun loveable outgoing dude, wanting to connect <3.  I hate flowers, but love doggies.'
+            }
+          </Text>
+        </Box>
+
+        <Table mt={8} size="sm">
+          <Thead>
+            <Tr>
+              <Th>Repo</Th>
+              <Th>Commits</Th>
+              <Th>Lines added</Th>
+              <Th>Lines removed</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {[
+              {
+                repo: 'web-app',
+                commits: '259',
+                added: '15,482',
+                removed: '6,185',
+              },
+              {
+                repo: 'foundation',
+                commits: '276',
+                added: '39,753',
+                removed: '24,805',
+              },
+              {
+                repo: 'graphql',
+                commits: '89',
+                added: '6,580',
+                removed: '4,789',
+              },
+              {
+                repo: 'marketing-site',
+                commits: '68',
+                added: '6,613',
+                removed: '899',
+              },
+              { repo: 'go-api', commits: '2', added: '93', removed: '37' },
+              { repo: 'terraform', commits: '4', added: '950', removed: '446' },
+            ].map(({ repo, commits, added, removed }) => {
+              return (
+                <Tr>
+                  <Td>{repo}</Td>
+                  <Td>{commits}</Td>
+                  <Td color="green.500">{added} ++</Td>
+                  <Td color="red.500">{removed} --</Td>
+                </Tr>
+              )
+            })}
+          </Tbody>
+        </Table>
+      </Box>
+    </Box>
+  )
+}
